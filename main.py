e84d623290032ea8b4231b71cda94a74e74ba8cd
chaos <liuyanchao99@gmail.com>
2019-09-12 18:05:23 +0800

    add result vg to git, this version can get better 3d model by polyfit

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ sys.path.append(r'E:\Workspace\Line2Plane\models')
 import LScene
 
 
-input_file=r'E:\Workspace\Line2Plane\data\cube.obj'
+input_file=r'E:\Workspace\Line2Plane\data\line_segments_3d_clustered_allpics_11_5_18.obj'
 output_file=input_file[:-3]+"vg"
 
 test=LScene.LScene()
 test.readObjFile(input_file)
-test.Cluster(2)
+test.Cluster(19)
 test.drawScene()
 
 print("writing output file")
