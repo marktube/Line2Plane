4ed3f0fae22a9b5b92a887fd2ad044e746201a43
yanchao <liuyanchao99@gmail.com>
2021-09-13 11:01:15 +0800

    add normal estimation

--- eval/RANSAC/main.cpp
+++ eval/RANSAC/main.cpp
@@ -70,15 +70,16 @@ int main(int argc, char **argv)
     }
 
     // ...
-    // don't forget to set the bbox in pc
+    // don't forget to calculate normals
+    pc.calcNormals(.005f*pc.getScale());
 
     RansacShapeDetector::Options ransacOptions;
-    ransacOptions.m_epsilon = .01f * pc.getScale();       // set distance threshold to .01f of bounding box width
+    ransacOptions.m_epsilon = .005f * pc.getScale();       // set distance threshold to .01f of bounding box width
                                                           // NOTE: Internally the distance threshold is taken as 3 * ransacOptions.m_epsilon!!!
     ransacOptions.m_bitmapEpsilon = .02f * pc.getScale(); // set bitmap resolution to .02f of bounding box width
                                                           // NOTE: This threshold is NOT multiplied internally!
-    ransacOptions.m_normalThresh = .9f;                   // this is the cos of the maximal normal deviation
-    ransacOptions.m_minSupport = 500;                     // this is the minimal numer of points required for a primitive
+    ransacOptions.m_normalThresh = .8f;                   // this is the cos of the maximal normal deviation
+    ransacOptions.m_minSupport = 10;                     // this is the minimal numer of points required for a primitive
     ransacOptions.m_probability = .001f;                  // this is the "probability" with which a primitive is overlooked
 
     RansacShapeDetector detector(ransacOptions); // the detector object
@@ -96,5 +97,6 @@ int main(int argc, char **argv)
                                                                                      // the points of shape i are found in the range
                                                                                      // [ pc.size() - \sum_{j=0..i} shapes[j].second, pc.size() - \sum_{j=0..i-1} shapes[j].second )
     std::cout << "remaining points: " << remaining << std::endl;
+    std::cout << shapes.size() << " primitives detected." << std::endl;
     return 0;
 }
