99ca45f3698259e647dedb80312965524e18b934
yanchao <liuyanchao99@gmail.com>
2022-02-15 15:35:52 +0800

    add vg2ply function

--- converter.py
+++ converter.py
@@ -3,6 +3,15 @@ import os
 import colorsys
 import numpy as np
 
+def get_colors(num_colors):
+    colors=[]
+    for i in np.arange(0., 360., 360. / num_colors):
+        hue = i / 360.
+        lightness = (50 + np.random.rand() * 10)/100.
+        saturation = (90 + np.random.rand() * 10)/100.
+        colors.append(colorsys.hls_to_rgb(hue, lightness, saturation))
+    return np.array(colors)
+
 def format2jpg(path_prefix):
     filenames = os.listdir(path_prefix)
     print(len(filenames))
@@ -58,9 +67,6 @@ def line2wire(fpath):
             fw.write('v %f %f %f\n'%(pxyz[i][0],pxyz[i][1],pxyz[i][2]))
         for i in range(len(lidx)):
             fw.write('f %d %d %d\n'%(lidx[i][0],lidx[i][1],lidx[i][1]))
-
-def vg2ply(fn):
-    pass
 
 def readVg(fn):
     pxyz = []
@@ -118,6 +124,43 @@ def readVg(fn):
 def writeVg(fn):
     pass
 
+def vg2ply(fn, outfn):
+    pxyz, gpara, gmemid = readVg(fn)
+    num_l = 0
+    for i in range(len(gmemid)):
+        num_l = num_l + len(gmemid[i])
+    num_l = num_l/2
+    colors = get_colors(len(gpara))
+    with open(outfn,'w') as fo:
+        fo.write("ply\n")
+        fo.write("format ascii 1.0\n")
+        fo.write("element vertex {}\n".format(len(pxyz)))
+        fo.write('property float x\n')
+        fo.write('property float y\n')
+        fo.write('property float z\n')
+        fo.write("element color {}\n".format(len(gpara)))
+        fo.write('property uchar red\n')
+        fo.write('property uchar green\n')
+        fo.write('property uchar blue\n')
+        fo.write('element edge %d\n'%(num_l))
+        fo.write('property int vertex1\n')
+        fo.write('property int vertex2\n')
+        fo.write('property int cid\n')
+        fo.write('end_header\n')
+
+        for i in range(len(pxyz)):
+            fo.write('{} {} {}\n'.format(pxyz[i][0],pxyz[i][1],pxyz[i][2]))
+
+        for i in range(len(gpara)):
+            fo.write('%d %d %d\n'%(colors[i][0]*255, colors[i][1]*255, colors[i][2]*255))
+
+        for j in range(len(gmemid)):
+            for i in range(len(gmemid[j])):
+                if i%2==0:
+                    fo.write('{} '.format(gmemid[j][i]))
+                else:
+                    fo.write('%d %d\n'%(gmemid[j][i],j))
+
 def combineVg(fn1,fn2,outfn):
     pxyz1, gpara1, gmemid1 = readVg(fn1)
     pxyz2, gpara2, gmemid2 = readVg(fn2)
@@ -161,9 +204,10 @@ def combineVg(fn1,fn2,outfn):
 
 
 if __name__ == '__main__':
-    combineVg('/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_cut6_7.vg',
+    '''combineVg('/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_cut6_7.vg',
               '/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_res.vg',
-              '/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_res.vg')
+              '/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_res.vg')'''
+    vg2ply('/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_res.vg','/home/hiko/Workspace/Line2Plane/data/Barn+haoyu_res.ply')
     #line2wire('/home/hiko/Workspace/11_1lines/20_sub/cmvs/Line3D++__W_FULL__N_10__sigmaP_2.5__sigmaA_10__epiOverlap_0.25__kNN_10__OPTIMIZED__vis_3.obj')
     #line2wire('/home/hiko/Downloads/LineData/Line3D++__office_c.obj')
     #line2wire('/home/hiko/Downloads/LineData/Line3D++__restaurant.obj')
