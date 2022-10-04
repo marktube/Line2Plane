53b017adf22f31c72e24d792f1d7118b0c454681
yanchao <liuyanchao99@gmail.com>
2022-10-04 16:40:05 +0800

    cluster for BK39_500_002053_0011

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ sys.path.append(os.path.join(BASE_DIR, 'models'))
 from models.LScene import LScene
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--volume', type=int, default=17, help='Volumes to use [default: volume 2]')
-parser.add_argument('--line_data', default='/home/hiko/Workspace/Line2Plane/data/BJ39_500_098050_0009/3dline_uncluster_song.obj', help='Line data input filepath [default: ]')
+parser.add_argument('--volume', type=int, default=7, help='Volumes to use [default: volume 2]')
+parser.add_argument('--line_data', default='/home/hiko/Workspace/Line2Plane/data/BK39_500_002053_0011/3dlines_cluster_song.obj', help='Line data input filepath [default: ]')
 parser.add_argument('--out', default='', help='Line data output filepath [default: {input filepath_[volume]}.vg]')
 parser.add_argument('--gui', default='true', help='true or false for showing graphic result [default: false]')
-parser.add_argument('--sr', default=1.0, help='sigma ratio can control the variance [default: 1.0]')
+parser.add_argument('--sr', default=8.0, help='sigma ratio can control the variance [default: 1.0]')
 FLAGS = parser.parse_args()
 
 NUM_VOLUMES=FLAGS.volume
